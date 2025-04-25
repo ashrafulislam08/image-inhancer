bce458b12ec6824589b961ced5e4b0b6239a7f2c
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-04-25 09:40:04 +0600

    Added loading spinner

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,6 +1,8 @@
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center h-full">Loading...</div>
+    <div className="flex items-center justify-center h-full">
+      <div className="animate-spin border-4 border-t-transparent w-10 h-10 scale-200 rounded-full border-red-300"></div>
+    </div>
   );
 };
 
